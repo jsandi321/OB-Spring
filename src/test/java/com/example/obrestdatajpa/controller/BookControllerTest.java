0648e4935487f6591d2fffe44e3172d4af2da26c
Jeison Sandi <54036161+jsandi321@users.noreply.github.com>
2022-07-27 13:01:42 -0600

    Testing de BookController

---
+++ src/test/java/com/example/obrestdatajpa/controller/BookControllerTest.java
@@ -0,0 +1,32 @@
+package com.example.obrestdatajpa.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @Test
+    void finAll() {
+    }
+
+    @Test
+    void findOneById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+}
